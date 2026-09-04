fda8a40dc90af27125e66fdd8faa2407a48941e3
Lukas <foxluc@gmail.com>
2026-09-04 11:12:00 +0200

    Refactor greeting for professionalism

---
+++ greet.js
@@ -0,0 +1,10 @@
+function ProperGreeter() {
+    reps = 10
+    name = ("Lukas")
+
+    for (i = 0; i < reps; i++) {
+        console.log("Greetings colleague " + name + "welcome to Lab #" + (reps - i))
+    }
+}
+
+ProperGreeter()
